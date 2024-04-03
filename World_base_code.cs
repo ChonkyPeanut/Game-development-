71f32a27732785fbeefa9c9b84e5330be6767e53
Chonky Peanut <89221482+ChonkyPeanut@users.noreply.github.com>
2024-04-03 12:19:46 -0400

    Update World_base_code.cs

--- World_base_code.cs
+++ World_base_code.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Collections.Generic;
 
-//character
 class Player
 {
   public string Name { get; set; }
   public string Role { get; set; }
   public int Age { get; set; }
 }
+
+class Location
+{
+  public string Name { get; set; }
+  public string Description {get; set; }
+}
+
